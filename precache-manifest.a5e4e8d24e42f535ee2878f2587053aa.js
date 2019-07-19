fd375079f641c3e0092b9d151538c70a17b1daaf
wangkangping <wangkangping@f-road.com.cn>
2019-07-19 13:20:09 -0700

    Updates

---
+++ precache-manifest.a5e4e8d24e42f535ee2878f2587053aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b55b5ad98ee66d168a6dd7d5fb4377c",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "9b6155ae8423d82f1b22",
+    "url": "/react-tutorial/static/css/main.1a942d58.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "9b6155ae8423d82f1b22",
+    "url": "/react-tutorial/static/js/main.dbc6b697.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
